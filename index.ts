db7cc6ff1bfe2c122e2f553e46b4c0af720dc2cf
Matthew Schleindl <mfsjr@Matthews-MacBook-Pro.local>
2018-01-26 07:19:02 -0500

    fix index.ts linting

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import {State} from "./src/types/State";
-import {ContainerComponent} from "./src/components/ContainerComponent";
-import {MappingAction, StateCrudAction} from "./src/actions/actions";
+import { State } from './src/types/State';
+import { ContainerComponent } from './src/components/ContainerComponent';
+import { MappingAction, StateCrudAction } from './src/actions/actions';
 
 export {
   State,
   ContainerComponent,
   StateCrudAction,
   MappingAction
-}
+};

c7faf427295a307651ce4e3a75c23157fde7b7a5
Matthew Schleindl <mfsjr@Matthews-MacBook-Pro.local>
2018-01-27 11:59:08 -0500

    add more objects to be exported

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-import { State } from './src/types/State';
+import { State, StateObject, StateConfigOptions } from './src/types/State';
 import { ContainerComponent } from './src/components/ContainerComponent';
-import { MappingAction, StateCrudAction } from './src/actions/actions';
+import { MappingAction, StateCrudAction, Action, DispatchType, ActionId } from './src/actions/actions';
 
 export {
   State,
+  StateObject,
+  StateConfigOptions,
   ContainerComponent,
+  ActionId,
+  Action,
+  DispatchType,
   StateCrudAction,
   MappingAction
 };

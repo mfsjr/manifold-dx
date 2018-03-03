8ae7edf0e9dbdc8da961407134eed5426e199ae8
Matthew Schleindl <mattschleindl@yahoo.com>
2018-03-03 18:36:39 -0500

    define MappingState values as arrays of mapping actions or another map of react keys, values are arrays of react elements

--- src/types/MappingState.ts
+++ src/types/MappingState.ts
@@ -1,6 +1,20 @@
 import { MappingAction } from '../actions/actions';
 
 /**
+ * Mapping actions require specific generic types, but common data structures need to hold 'any'
+ */
+/* tslint:disable:no-any */
+export type GenericMappingAction = MappingAction<any, any, any, any>;
+/* tslint:enable:no-any */
+
+/**
+ * Path strings map to values defined by this type, which can refer to simple properties having
+ * multiple React components that they update, or to lists whose keys refer to one or more
+ * React components that get updated when that row/key change.
+ */
+export type PathMappingValue = GenericMappingAction[] | Map<React.Key, GenericMappingAction[]>;
+
+/**
  * Relates application state properties with React components, for the purpose of
  * forcing components to update (ie render).
  *
@@ -9,7 +23,7 @@ import { MappingAction } from '../actions/actions';
  */
 export class MappingState {
     /* tslint:disable:no-any */
-  private pathMappings = new Map<string, MappingAction<any, any, any, any>[]>();
+  private pathMappings = new Map<string, PathMappingValue>();
     /* tslint:enable:no-any */
 
   /**
@@ -19,16 +33,41 @@ export class MappingState {
   public getSize(): number {
     return this.pathMappings.size;
   }
-    /* tslint:disable:no-any */
-  public getPathMappings(propFullPath: string): MappingAction<any, any, any, any>[] | undefined {
-      /* tslint:enable:no-any */
-    return this.pathMappings.get(propFullPath);
+
+  getMappingActions(path: string): GenericMappingAction[] | undefined {
+    let pathResults = this.pathMappings.get(path);
+    if (!pathResults) {
+      return undefined;
+    }
+    if (pathResults instanceof Array) {
+      return pathResults as GenericMappingAction[] | undefined;
+    } else {
+      throw Error(`pathResults from ${path} expected to be instanceof Array`);
+    }
+  }
+
+  getArrayMappingActions(path: string, key: string | number ): GenericMappingAction[] | undefined {
+    let pathResults = this.pathMappings.get(path);
+    if (!pathResults) {
+      return undefined;
+    }
+    if (pathResults instanceof Map) {
+      return pathResults.get(key);
+    } else {
+      throw new Error(`pathResults from ${path} expected to be instanceof Map`);
+    }
   }
 
     /* tslint:disable:no-any */
-  public getOrCreatePathMappings(propFullPath: string): MappingAction<any, any, any, any>[] {
+  public getPathMappings(propFullPath: string): GenericMappingAction[] | undefined {
       /* tslint:enable:no-any */
-    let result = this.pathMappings.get(propFullPath);
+    return this.getMappingActions(propFullPath);
+  }
+
+    /* tslint:disable:no-any */
+  public getOrCreatePathMappings(propFullPath: string): GenericMappingAction[] {
+      /* tslint:enable:no-any */
+    let result = this.getMappingActions(propFullPath);
     if (!result) {
       result = [];
       this.pathMappings.set(propFullPath, result);
@@ -43,8 +82,8 @@ export class MappingState {
    * @returns {number} index at which the component was removed, -1 if not found
    */
     /* tslint:disable:no-any */
-  public removePathMapping(_fullPath: string, container: MappingAction<any, any, any, any>): number {
-    let containers: MappingAction<any, any, any, any>[] | undefined = this.pathMappings.get(_fullPath);
+  public removePathMapping(_fullPath: string, container: GenericMappingAction): number {
+    let containers: GenericMappingAction[] | undefined = this.getMappingActions(_fullPath);
       /* tslint:enable:no-any */
     if (containers) {
       let index = containers.indexOf(container);

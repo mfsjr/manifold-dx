4d934aaff87b085dc4d0e5f028f139050b815477
Matthew Schleindl <mattschleindl@yahoo.com>
2018-06-14 06:18:22 -0400

    merge with latest, bump version to 0.5.9, rebuild dist

---
+++ dist/src/actions/actionCreators.d.ts
@@ -0,0 +1,97 @@
+import { StateObject } from '../';
+import { ArrayChangeAction, DispatchType, MappingAction, StateAction, StateCrudAction } from './actions';
+import { ContainerComponent } from '../components/ContainerComponent';
+export declare type NotArray<T> = T;
+export declare function isNotArray<T>(value: T): value is NotArray<T>;
+/**
+ * Create CRUD actions for properties of a StateObject.
+ * Array CRUD actions are in {@link ArrayActionCreator}
+ */
+export declare class ActionCreator<S extends StateObject> {
+    private parent;
+    constructor(parent: S);
+    protected getPropertyKeyForValue<V>(value: V): keyof S;
+    protected throwIfArray<K extends keyof S>(propValue: S[K]): void;
+    rerender<K extends keyof S>(propertyKey: K): StateCrudAction<S, K>;
+    insert<K extends keyof S>(propertyKey: K, value: S[K]): StateCrudAction<S, K>;
+    update<K extends keyof S>(propertyKey: K, value: S[K]): StateCrudAction<S, K>;
+    /**
+     * Delete the property (named 'remove' because 'delete' is a reserved word)
+     * @param {K} propertyKey
+     * @returns {Action}
+     */
+    remove<K extends keyof S>(propertyKey: K): StateCrudAction<S, K>;
+    insertStateObject<K extends keyof S>(value: S[K] & StateObject, propertyKey: K): StateCrudAction<S, K>;
+    removeStateObject<K extends keyof S>(propertyKey: K): StateCrudAction<S, K>;
+}
+/**
+ * Factory method for CrudActionCreator, rather than exposing implementation details
+ * @param {S} parent
+ * @returns {ActionCreator<S extends StateObject>}
+ */
+export declare function getActionCreator<S extends StateObject>(parent: S): ActionCreator<S>;
+export declare function getArrayActionCreator<S extends StateObject, K extends keyof S, V extends Object>(parent: S, childArray: Array<V> & S[K]): ArrayActionCreator<S, K, V>;
+/**
+ * Class for creating CRUD actions for arrays of objects (not primitives).
+ *
+ * Arrays of primitives can be handled with CRUD operations that treat arrays as simple properties,
+ * using {@link ActionCreator}s above.  Note that the creation and deletion of arrays of
+ * objects would need to use the same.
+ *
+ * usage example from tests:  new ArrayCrudActionCreator(nameState, nameState.addresses, streetKeyFn)
+ *
+ * S is the StateObject which the array is a property of
+ */
+export declare class ArrayActionCreator<S extends StateObject, K extends keyof S, V extends Object> {
+    private parent;
+    private propertyKey;
+    private valuesArray;
+    /**
+     * Construct an array crud creator.  We require a somewhat redundant 'valuesArray'
+     * parameter in order to provide TypeScript with a strongly typed object that
+     * we can use in conjunction with a typeguard so that the array element's property is an
+     * appropriately typed value.
+     *
+     * There may be some TS experts out there who know how to do this, but this appears
+     * to be outside of the capabilities of 2.7 judging by the docs.
+     *
+     * Recent PR's targeted for 2.8 may change this, see https://github.com/Microsoft/TypeScript/pull/21496
+     *
+     * S extends StateObject
+     *
+     * @param {S} parent
+     * @param {keyof S} propertyKey
+     * @param {Array<V>} childArray
+     * @param {ArrayKeyGeneratorFn} keyGenerator
+     */
+    constructor(parent: S, childArray: Array<V> & S[K]);
+    insertArray(newArray: Array<V> & S[K]): StateAction<S, K>;
+    removeArray(): StateAction<S, K>;
+    rerenderArray(): StateAction<S, K>;
+    appendElement(value: V): StateAction<S, K>;
+    /**
+     * Insert into the StateObject's array, and return an array of actions for each element above the insertion.
+     *
+     * Mapping actions will remain unchanged, but the value of all the mapped state, and container view properties will
+     * be updated.
+     *
+     * If the additional object at the end of the array is to be shown, an additional mapping action would have to be
+     * performed, a {@link ContainerComponent} would be required, etc.
+     *
+     *
+     * @param {number} index
+     * @param {V} value
+     * @returns {Action}
+     */
+    insertElement(index: number, value: V): StateAction<S, K>[];
+    rerenderElement(index: number): StateAction<S, K>;
+    updateElement(index: number, newValue: V): ArrayChangeAction<S, K, V>;
+    removeElement(index: number): StateAction<S, K>[];
+}
+export interface ArrayMappingCreatorOptions<S extends StateObject, K extends keyof S, E> {
+    array: Array<E> & S[K];
+}
+export declare function getMappingActionCreator<S extends StateObject, K extends keyof S, A extends StateObject, E>(_parent: S, _propKey: K, arrayOptions?: ArrayMappingCreatorOptions<S, K, E>): {
+    createPropertyMappingAction: <CP, VP, TP extends keyof VP>(_component: ContainerComponent<CP, VP, A>, targetPropKey: TP, ...dispatches: DispatchType[]) => MappingAction<S, K, CP, VP, TP, A, E>;
+    createArrayIndexMappingAction: <CP, VP, TP extends keyof VP>(index: number, _component: ContainerComponent<CP, VP, A>, targetPropKey: TP, ...dispatches: DispatchType[]) => MappingAction<S, K, CP, VP, TP, A, E>;
+};

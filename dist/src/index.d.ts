6854cb2fccf4290f445b3b249bd530d5437f4594
Matthew Schleindl <mattschleindl@yahoo.com>
2019-10-13 11:44:49 -0400

    update readme, expose Manager in index.ts, rebuild and bump version

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -4,9 +4,10 @@ import { RenderPropsComponent, ContainerRenderProps } from './components/RenderP
 import { Store, StateObject, State, StateParent, StateProp } from './types/Store';
 import { ArrayChangeAction } from './actions/actions';
 import { getActionCreator, getArrayActionCreator, getMappingActionCreator } from './actions/actionCreators';
+import { Manager } from './types/Manager';
 /**
  * This is intended to be a list of objects you will need to use the library as-is.
  * For more advanced usage, others are available via direct reference, and examples
  * of usage should be in the unit tests.
  */
-export { Store, State, StateParent, StateProp, StateObject, Action, getActionCreator, getArrayActionCreator, getMappingActionCreator, StateCrudAction, ArrayChangeAction, MappingAction, AnyMappingAction, MappingHook, ContainerComponent, RenderPropsComponent, ContainerRenderProps };
+export { Store, State, StateParent, StateProp, StateObject, Action, getActionCreator, getArrayActionCreator, getMappingActionCreator, StateCrudAction, ArrayChangeAction, Manager, MappingAction, AnyMappingAction, MappingHook, ContainerComponent, RenderPropsComponent, ContainerRenderProps };
